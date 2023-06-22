057acb130138de6d334e92c0dedec090d9d55444
murakijun <murakijunjun0412@yahoo.co.jp>
2023-06-23 02:25:46 +0900

    04

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,10 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+    
+    protected $fillable = [
+        'title',
+        'body',
+    ];
+    
     use HasFactory;
-    //public function getByLimit(int $limit_count = 10)
-    //{
+    public function getByLimit(int $limit_count = 10)
+    {
     // updated_atで降順に並べたあと、limitで件数制限をかける
-    //    return $this->orderBy('updated_at', 'DESC')->limit($limit_count)->get();
-    //}
+        return $this->orderBy('updated_at', 'DESC')->limit($limit_count)->get();
+    }
 }
