91e78f8ac62d6ff08d59d6a2045521e5b12559b8
murakijun <murakijunjun0412@yahoo.co.jp>
2023-06-23 21:27:19 +0900

    8-5

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -14,10 +14,6 @@ class PostController extends Controller
 //     return view('posts.index')->with(['posts' => $post->getByLimit()]);
      return view('posts.index')->with(['posts' => $post->getPaginateByLimit()]);
     }
-    public function show(Post $post)
-    {
-         return view('posts.show')->with(['post' => $post]);
-    }
     
     public function show(Post $post)
     {
@@ -34,6 +30,19 @@ class PostController extends Controller
     {
         $input = $request['post'];
         $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
+    }
+    
+    public function edit(Post $post)
+    {
+        return view('posts.edit')->with(['post' => $post]);
+    }
+    
+    public function update(PostRequest $request, Post $post)
+    {
+        $input_post = $request['post'];
+        $post->fill($input_post)->save();
+    
         return redirect('/posts/' . $post->id);
     }
 }
